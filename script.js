68cedd877e6edc064a5c696109d2aa9302a3cedc
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-09-08 22:43:26 +0530

    assignment done

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// const hamburger = document.querySelector(".hamburger");
-// const menu = document.querySelector(".menu");
 
-// hamburger.addEventListener("click", () => {
-//   if (menu.style.display === "none" || menu.style.display === "") {
-//     menu.style.display = "block"; // Show the menu
-//   } else {
-//     menu.style.display = "none"; // Hide the menu
-//   }
-// });
-
-// let navToggle = document.querySelector(".hamburger");
-// let bars = document.querySelectorAll(".bar");
-
-// function toggleHamburger(e) {
-//   bars.forEach((bar) => bar.classList.toggle("x"));
-// }
-
-// navToggle.addEventListener("click", toggleHamburger);
 
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
